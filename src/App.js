45d7ea000293465cff9c2598ef3187a3a5d3a4bc
THONWELLING <thonwellingdani13@gmail.com>
2021-05-25 11:37:24 -0300

    add pasta components e MovieColumn.js/.css p exibição de filmes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { useEffect } from 'react';  // 
+import React, { useEffect, useState } from 'react';  // o use state foi usado para salvar a lista
 import Tmdb from './Tmdb';
+import MovieColumn from'./components/MovieColumn';
 
 
 export default () => {
+  const [movieList, setMovieList] = useState([]);     //aqui basicamente eu disse  que minha constante é o movieList e que é para setar  essa constante com o useState
+
   useEffect(() => {
     
     const loadAll = async () => {
       // pegando a lista total dos items
         let list = await Tmdb.getHomeList();
         
-        console.log(list);
+        setMovieList(list);                        //aqui foi carregada ou setada toda a lista
     }
     loadAll();
   }, []);
   return (
-    <div>HELLO WORLD!!</div>
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item,key)=>(              //vou dar um loop/mapear o  movieList e usar uam função  para exibir é necessária  em todo loop o uso de uma key
+          <div>
+            <MovieColumn key={key} title={item.title} items={item.items} />                                
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }
---
+++ src/components/MovieColumn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './MovieColumn.css';
+
+export default ({title, items}) => {
+    return(
+        <div>
+            <h2>{title}</h2>
+        </div>
+    );
+}
