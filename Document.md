1e79ddf0cc1bbacb47b99212546e172fe1c9fc82
THONWELLING <thonwellingdani13@gmail.com>
2021-05-26 00:16:30 -0300

    add componente para filme em destaque e arquivo descritivo

--- Document.md
+++ Document.md
@@ -15,4 +15,11 @@ no arquivo MovieColumn.js  na linha 9 foi criado um loop  dentro do loop das lis
 {items.results.length > 0 && items.results.map((item, key)=>(
     <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
 ))}
-básicamente quer dizer se o resultado de items for maior que 0 então mapeie so resultados e mostre os (items usando a key)
+básicamente quer dizer se o resultado de items for maior que 0 então mapeie so resultados e mostre os (items usando a key) 
+para eu ter esse tipo de corrossel que vai para um lado e para o outro  eu preciso de 2 divs  uma vai ser  a área total e a outra vai ser responável por ter todos os items dentro e essa segunda div que eu movimento atravé da margem dela .
+
+ foi add em MovieColumn as 2 divs mensionadas acima MovieColumn--Movies para a area total e MovieColumn--Item para as imagens/conteúdos
+ depois de fazer  um pouco de eslilização na area dos filmes.
+ criei mais 1 Componente FeaturedMovie.js/css   para controlar  a parte  do filme em destaque ou o FeaturedMovie.
+ importei  esse arquivo  no App.js
+  no App.js  eu criei  uma constante chamada featuredData e usei um useState  especifico p ele quando ele for preenchido  é que vai ser exibido.
--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import React, { useEffect, useState } from 'react';  // o use state foi usado pa
 import Tmdb from './Tmdb';
 import MovieColumn from'./components/MovieColumn';
 import './App.css';
+import FeaturedMovie from './components/FeaturedMovie';
 
 
 export default () => {
-  const [movieList, setMovieList] = useState([]);     //aqui basicamente eu disse  que minha constante é o movieList e que é para setar  essa constante com o useState
 
+  const [movieList, setMovieList] = useState([]);          //aqui basicamente eu disse  que minha constante é o movieList e que é para setar  essa constante com o useState
+  const [featuredData, setFeaturedData] = useState(null);    
   useEffect(() => {
     
     const loadAll = async () => {
+    
       // pegando a lista total dos items
-        let list = await Tmdb.getHomeList();
-        
-        setMovieList(list);                        //aqui foi carregada ou setada toda a lista
+
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);                        //aqui foi carregada ou setada toda a lista
+      // pegando  o featuredData
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
+
     loadAll();
   }, []);
+
   return (
     <div className="page">
+      {featuredData &&
+      <FeaturedMovie item={featuredData} />
+      }
       <section className="lists">
         {movieList.map((item,key)=>(              //vou dar um loop/mapear o  movieList e usar uam função  para exibir é necessária  em todo loop o uso de uma key
           <div>
